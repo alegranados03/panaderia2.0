40b0d38c89c2df5983ea557952a89b68809075dc
Mego1596 <maurigranados1596@gmail.com>
2018-12-19 14:56:57 -0600

    Update usuario.blade.php

--- resources/views/administracion/email/usuario.blade.php
+++ resources/views/administracion/email/usuario.blade.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Usuario y contraseña</title>
 </head>
 <body>
-
+    <h2>Usuario y contraseña</h2>
+    <h2>Clinica YekixPaki </h2>
+    <p>
+        Usuario: {{$user->name}} <br>
+        Contraseña: {{$user->password}}<br>
+    </p>
+    <strong> Almacene la clave en un lugar seguro </strong>
+    <br>
+    <cite>Saludos, clinica Yekixpaki </cite>
 </body>
 </html>
