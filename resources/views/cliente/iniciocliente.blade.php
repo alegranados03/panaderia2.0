630141a108ac7cc96f385672f9de5e6ef215c5c8
Mego1596 <maurigranados1596@gmail.com>
2018-12-20 12:54:54 -0600

    Actualizacion de Choque de Links

--- resources/views/cliente/iniciocliente.blade.php
+++ resources/views/cliente/iniciocliente.blade.php
@@ -4,6 +4,16 @@
 Tienda
 @endsection
 
+@section('links')
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/bootstrap4/bootstrap.min.css')}}">
+<link href="{{asset('OneTech/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/OwlCarousel2-2.2.1/animate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/slick-1.8.0/slick.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/main_styles.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/responsive.css')}}">
+@endsection
 @section('content')
 
   <div class="banner">
--- resources/views/layouts/baseCliente.blade.php
+++ resources/views/layouts/baseCliente.blade.php
@@ -6,16 +6,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="OneTech shop project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/bootstrap4/bootstrap.min.css')}}">
-<link href="{{asset('OneTech/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/OwlCarousel2-2.2.1/animate.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/plugins/slick-1.8.0/slick.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/main_styles.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/responsive.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/product_styles.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/product_responsive.css')}}">
+@yield('links')
+
+<!--<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/product_styles.css')}}">-->
+<!--<link rel="stylesheet" type="text/css" href="{{asset('OneTech/styles/product_responsive.css')}}">-->
+ 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 </head>
